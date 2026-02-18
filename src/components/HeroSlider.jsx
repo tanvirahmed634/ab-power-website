58c77f731862183d0638ef77e16dbc36ad68c70b
Tanvir Ahmed <tanvir.ta634@outlook.com>
2026-02-18 23:08:22 +0600

    Rename png to PNG in code for Vercel build

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 import slide1 from "../assets/slide1.jpeg"
 import slide2 from "../assets/slide2.jpg"
 import slide3 from "../assets/slide3.jpg"
-import slide4 from "../assets/slide4.PNG"
+import slide4 from "../assets/slide4.png"
 
 export default function HeroSlider() {
 
